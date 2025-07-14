f9744258dce6a1fc74eb28e9785f2da5cbee8e78
NicoGan <nicogan1707@yahoo.com.ar>
2025-07-14 11:40:09 -0300

    Agregar recuperación de metadatos de usuario en el componente Profile

--- client/src/maincomponentes/Profile.js
+++ client/src/maincomponentes/Profile.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  const [userMetadata, setUserMetadata] = useState(null);
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "dev-ltnc4wqr1q2lyk78.us.auth0.com"; //Revisar si se debe guardar en una variable de entorno
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: `https://${domain}/api/v2/`,
+            scope: "read:current_user",
+          },
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    if (isAuthenticated) {
+      getUserMetadata();
+    }
+  }, [getAccessTokenSilently, user?.sub, isAuthenticated]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -18,6 +52,12 @@ const Profile = () => {
           style={{ width: 80 }}
         />
         <p>{user.email}</p>
+        <h3>User Metadata</h3>
+        {userMetadata ? (
+          <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+        ) : (
+          "No user metadata defined"
+        )}
       </div>
     )
   );
